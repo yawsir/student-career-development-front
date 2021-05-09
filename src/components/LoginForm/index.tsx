d4dcb53bca5165422da21f94ecd7995c01f3a21c
yuyang <yuy@nroad.com.cn>
2021-05-09 16:34:56 +0800

    feat: 🛎 教师/学生登录页面

--- src/components/LoginForm/index.tsx
+++ src/components/LoginForm/index.tsx
@@ -1,46 +1,123 @@
 /*
  * @Author: yuyang
  * @Date: 2021-05-05 20:01:53
- * @LastEditTime: 2021-05-05 20:42:43
+ * @LastEditTime: 2021-05-09 16:32:54
  * @LastEditors: yuyang
  */
 import React from 'react';
 import { Form, Input, Button } from 'antd';
 import { UserOutlined, KeyOutlined } from '@ant-design/icons';
+import logo from '@/assets/logo.png';
 import styles from './index.less';
 
-interface LoginFormProps {
-  onLogin?: () => void;
+export type ClientName = 'student' | 'teacher';
+export interface LoginFormValues {
+  username: string;
+  password: string;
+  verifyCode: string;
 }
-const LoginForm: React.FC<LoginFormProps> = (props) => {
-  const { onLogin } = props;
+interface LoginFormProps {
+  clientName: ClientName;
+  onLogin?: (values: LoginFormValues) => void;
+  onResetPassword?: () => void;
+  onRegister?: () => void;
+  onSwitchClient?: (currentClient: ClientName) => void
+}
 
-  const handleFinish = () => {
-    onLogin?.();
+const LoginForm: React.FC<LoginFormProps> = (props) => {
+  const { onLogin, clientName, onResetPassword, onRegister, onSwitchClient } = props;
+  const title = clientName === 'student' ? '学生登录 Student Login' : '教师登录 Teacher Login';
+  const switchBtnText = clientName === 'student' ? '教师端切换' : '学生端切换';
+  const handleFinish = (values: LoginFormValues) => {
+    onLogin?.(values);
   };
+  const handleResetPassword = () => {
+    onResetPassword?.();
+  };
+
+  const handleRegister = () => {
+    onRegister?.();
+  };
+
+  const handleSwitchClient = () => {
+    onSwitchClient?.(clientName);
+  };
+
   return (
     <div className={styles['login-form']}>
-      <div className={styles['login-form__bg']}>
-        <div className={styles['login-form__wrap']}>
-          <Form
-            onFinish={handleFinish}
-          >
-            <Form.Item
-              name="username"
-            >
-              <Input prefix={<UserOutlined />} />
-            </Form.Item>
-            <Form.Item
-              name="password"
-            >
-              <Input.Password prefix={<KeyOutlined />} />
-            </Form.Item>
-            <Form.Item>
-              <Button htmlType="submit">登录</Button>
-            </Form.Item>
-          </Form>
+      <div className={styles['login-form__bg']} />
+      <div className={styles['login-form__wrap']}>
+        <div className={styles['login-form__logo']}>
+          <div className={styles['login-form__logo__image']}>
+            <img src={logo} alt="" />
+          </div>
+          <p className={styles['login-form__logo__text']}>深圳学生生涯发展支持平台</p>
         </div>
 
+        <div className={styles['login-form__content']}>
+          <h5 className={styles['login-form__content__title']}>{title}</h5>
+          <div className={styles['login-form__content__form']}>
+            <Form
+              onFinish={handleFinish}
+            >
+              <Form.Item
+                name="username"
+              >
+                <Input
+                  prefix={<UserOutlined className={styles['login-form__content__form__icon']} />}
+                  className={styles['login-form__content__form__input']}
+                />
+              </Form.Item>
+              <Form.Item
+                name="password"
+              >
+                <Input.Password
+                  prefix={<KeyOutlined className={styles['login-form__content__form__icon']} />}
+                  className={styles['login-form__content__form__input']}
+                />
+              </Form.Item>
+              <Form.Item
+                name="verifyCode"
+              >
+                <Input
+                  className={styles['login-form__content__form__input']}
+                />
+              </Form.Item>
+              <Form.Item>
+                <Button htmlType="submit" type="primary" className={styles['login-form__content__form__btn']}>登录</Button>
+              </Form.Item>
+              <Form.Item>
+                <div className={styles['login-form__content__form__links']}>
+                  <div className={styles['login-form__content__form__links__group']}>
+                    <span
+                      className={styles['login-form__content__form__links__link']}
+                      onClick={handleResetPassword}
+                    >
+                      忘记密码
+
+                    </span>
+                  </div>
+                  <div className={styles['login-form__content__form__links__group']}>
+                    <span
+                      className={styles['login-form__content__form__links__link']}
+                      onClick={handleRegister}
+                    >
+                      立即注册
+
+                    </span>
+                    <span
+                      className={`${styles['login-form__content__form__links__link']} ${styles['login-form__content__form__links__spec']}`}
+                      onClick={handleSwitchClient}
+                    >
+                      {switchBtnText}
+
+                    </span>
+                  </div>
+                </div>
+              </Form.Item>
+            </Form>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/pages/student/login/index.tsx
+++ src/pages/student/login/index.tsx
@@ -1,16 +1,34 @@
 /*
  * @Author: yuyang
  * @Date: 2021-05-05 20:04:29
- * @LastEditTime: 2021-05-05 20:23:11
+ * @LastEditTime: 2021-05-09 16:33:49
  * @LastEditors: yuyang
  */
 import React from 'react';
-import LoginForm from '@/components/LoginForm';
+import { history } from 'umi';
+import LoginForm, { LoginFormValues, ClientName } from '@/components/LoginForm';
 
-const Login: React.FC = () => (
-  <div>
-    <LoginForm />
-  </div>
-);
+const Login: React.FC = () => {
+  const handleLogin = (values: LoginFormValues) => {
+    console.log(values);
+  };
+
+  const handleSwitchClient = (currentClient: ClientName) => {
+    if (currentClient === 'student') {
+      history.push('/teacher/login');
+    } else {
+      history.push('/student');
+    }
+  };
+  return (
+    <div>
+      <LoginForm
+        clientName="student"
+        onLogin={handleLogin}
+        onSwitchClient={handleSwitchClient}
+      />
+    </div>
+  );
+};
 
 export default Login;
