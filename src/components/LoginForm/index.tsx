27d91506c6f8ec1a85670ad62072957bbd4aa687
yuyang <yuy@nroad.com.cn>
2021-07-08 18:01:45 +0800

    style: 🎀 login

--- src/components/LoginForm/index.tsx
+++ src/components/LoginForm/index.tsx
@@ -1,13 +1,12 @@
 /*
  * @Author: yuyang
  * @Date: 2021-05-05 20:01:53
- * @LastEditTime: 2021-07-07 13:55:11
+ * @LastEditTime: 2021-07-08 17:39:31
  * @LastEditors: yuyang
  */
 import React from 'react';
 import { Form, Input, Button } from 'antd';
 import { UserOutlined, KeyOutlined } from '@ant-design/icons';
-import logo from '@/assets/logo.png';
 import styles from './index.less';
 
 export interface LoginFormValues {
@@ -30,72 +29,55 @@ const LoginForm: React.FC<LoginFormProps> = (props) => {
   };
 
   return (
-    <div className={styles['login-form']}>
-      <div className={styles['login-form__bg']} />
-      <div className={styles['login-form__wrap']}>
-        <div className={styles['login-form__logo']}>
-          <div className={styles['login-form__logo__image']}>
-            <img src={logo} alt="" />
-          </div>
-          <p className={styles['login-form__logo__text']}>深圳学生生涯发展支持平台</p>
-        </div>
-
-        <div className={styles['login-form__content']}>
-          <h5 className={styles['login-form__content__title']}>登录 Login</h5>
-          <div className={styles['login-form__content__form']}>
-            <Form
-              onFinish={handleFinish}
+    <Form
+      onFinish={handleFinish}
+    >
+      <Form.Item
+        name="username"
+        rules={[
+          {
+            required: true,
+            message: '请输入用户名',
+          },
+        ]}
+      >
+        <Input
+          prefix={<UserOutlined className={styles['login-form__content__form__icon']} />}
+          className={styles['login-form__content__form__input']}
+          placeholder="用户名"
+        />
+      </Form.Item>
+      <Form.Item
+        name="password"
+        rules={[
+          {
+            required: true,
+            message: '请输入密码',
+          },
+        ]}
+      >
+        <Input.Password
+          prefix={<KeyOutlined className={styles['login-form__content__form__icon']} />}
+          className={styles['login-form__content__form__input']}
+          placeholder="密码"
+        />
+      </Form.Item>
+      <Form.Item>
+        <Button htmlType="submit" type="primary" className={styles['login-form__content__form__btn']}>登录</Button>
+      </Form.Item>
+      <Form.Item>
+        <div className={styles['login-form__content__form__links']}>
+          <div className={styles['login-form__content__form__links__group']}>
+            <span
+              className={styles['login-form__content__form__links__link']}
+              onClick={handleResetPassword}
             >
-              <Form.Item
-                name="username"
-                rules={[
-                  {
-                    required: true,
-                    message: '请输入用户名',
-                  },
-                ]}
-              >
-                <Input
-                  prefix={<UserOutlined className={styles['login-form__content__form__icon']} />}
-                  className={styles['login-form__content__form__input']}
-                  placeholder="用户名"
-                />
-              </Form.Item>
-              <Form.Item
-                name="password"
-                rules={[
-                  {
-                    required: true,
-                    message: '请输入密码',
-                  },
-                ]}
-              >
-                <Input.Password
-                  prefix={<KeyOutlined className={styles['login-form__content__form__icon']} />}
-                  className={styles['login-form__content__form__input']}
-                  placeholder="密码"
-                />
-              </Form.Item>
-              <Form.Item>
-                <Button htmlType="submit" type="primary" className={styles['login-form__content__form__btn']}>登录</Button>
-              </Form.Item>
-              <Form.Item>
-                <div className={styles['login-form__content__form__links']}>
-                  <div className={styles['login-form__content__form__links__group']}>
-                    <span
-                      className={styles['login-form__content__form__links__link']}
-                      onClick={handleResetPassword}
-                    >
-                      忘记密码
-                    </span>
-                  </div>
-                </div>
-              </Form.Item>
-            </Form>
+              忘记密码
+            </span>
           </div>
         </div>
-      </div>
-    </div>
+      </Form.Item>
+    </Form>
   );
 };
 
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: yuyang
  * @Date: 2021-05-05 20:04:29
- * @LastEditTime: 2021-07-07 13:52:49
+ * @LastEditTime: 2021-07-08 18:01:31
  * @LastEditors: yuyang
  */
 import React from 'react';
@@ -24,11 +24,20 @@ const Login: React.FC = () => {
   };
 
   return (
-    <div>
-      <LoginForm
-        onLogin={handleLogin}
-        onResetPassword={handleResetPassword}
-      />
+    <div className="container h-full mx-auto flex p-2">
+      <div className="flex flex-col p-2">
+        <div>
+          <LoginForm
+            onLogin={handleLogin}
+            onResetPassword={handleResetPassword}
+          />
+        </div>
+        <div className="bg-red-200 h-96">资讯</div>
+        <div className="bg-green-200  h-96">资讯</div>
+      </div>
+      <div className="bg-blue-200 h-screen">
+        资讯
+      </div>
     </div>
   );
 };
