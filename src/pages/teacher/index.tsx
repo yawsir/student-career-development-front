eb9b49868ef26fc557c5becb5102eee0bddaefcb
haoyh <haoyh@nroad.com.cn>
2021-10-15 11:39:11 +0800

    teacher 优化

--- src/pages/teacher/index.tsx
+++ src/pages/teacher/index.tsx
@@ -1,39 +1,40 @@
 /*
  * @Author: yuyang
  * @Date: 2021-05-05 20:01:53
- * @LastEditTime: 2021-10-14 15:53:13
+ * @LastEditTime: 2021-10-15 11:25:31
  * @LastEditors: haoyh
  */
-import React, { useState } from 'react';
+import React from 'react';
 import NewsContainer from '@/components/NewsContainer';
-import MultiPane from '@/components/MultiPane';
-import NewsList from '@/components/NewsList';
 import background from '@/assets/teacher/background.png';
 // import TipModal from './components/TipModal';
-import { tabs1, tabs2, tabs3, tabs4, tabs5 } from './news-list';
-import styles from './index.less';
 import TeacherCard from './components/TeacherCard';
+import styles from './index.less';
 
-const Teacher: React.FC = () => {
-  const tabsList = [tabs1, tabs2, tabs3, tabs4, tabs5];
-  const bgColors = ['#F5EA8C', '#F5B48C', '#BCF48C', '#8DF4EE', '#FBA4C5'];
-  const [currentPane, setCurrentPane] = useState('p0');
-  return (
-    <NewsContainer>
-      <div className="container mx-auto py-2">
-        <div className="w-full mx-auto" style={{ backgroundImage: `url(${background})` }}>
-          <div className="flex justify-center items-center text-center mb-8">
-            <span className="text-xl text-title my-5 rounded-md font-bold transition transform scale-100 hover:scale-110 bg-gradient-to-r from-bg-1 to-bg-2" style={{ padding: '16px 50px' }}>
-              名师专栏
-            </span>
-          </div>
-          <div className="w-2/3 mx-auto flex flex-wrap justify-between px-14">
-            <TeacherCard />
-          </div>
+const Teacher: React.FC = () => (
+  <NewsContainer background={background}>
+    <div className={`${styles.teacherPage} container mx-auto py-2`}>
+      <div className="w-full mx-auto">
+        <div className="flex justify-center items-center text-center my-4">
+          <span className="text-xl text-title my-5 rounded-md font-bold transition transform scale-100 hover:scale-110 bg-gradient-to-r from-bg-1 to-bg-2" style={{ padding: '16px 50px' }}>
+            名师专栏
+          </span>
         </div>
+        <div className={`${styles['teacher-content']} mx-auto flex flex-wrap justify-between`}>
+          <TeacherCard />
+        </div>
+      </div>
+      <div className="w-full">
+        <div className="my-5 rounded-md font-bold  bg-gradient-to-r from-bg-1 to-bg-2">
+          <span>
+            专业资料库
+          </span>
+        </div>
+      </div>
+    </div>
 
-        {/* <TipModal modalTitle={<div className="text-gray-500 text-xl">温馨提示</div>} /> */}
-        {/* <div className="w-4/5 mx-auto bg-white py-4 border-2 border-b-0 border-solid border-white">
+    {/* <TipModal modalTitle={<div className="text-gray-500 text-xl">温馨提示</div>} /> */}
+    {/* <div className="w-4/5 mx-auto bg-white py-4 border-2 border-b-0 border-solid border-white">
           <h3 className="border-0 border-b border-solid border-gray-400 text-center pb-8">
             <span className="inline-block px-16 py-2 text-3xl border-0 border-b-2 border-solid border-primary">专业资料库</span>
           </h3>
@@ -78,8 +79,7 @@ const Teacher: React.FC = () => {
             </MultiPane>
           </div>
         </div> */}
-      </div>
-    </NewsContainer>
-  );
-};
+    {/* </div> */}
+  </NewsContainer>
+);
 export default Teacher;
